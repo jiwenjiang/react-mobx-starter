6a90ae40c70888d79dcd94c0d81de3dd62b54018
513755565@qq.com <513755565@qq.com>
2019-01-26 10:32:08 +0800

    pull codes

---
+++ src/component/common/routeError/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by j_bleach on 2019/1/10 0010.
+ */
+import React from "react";
+import "./index.less";
+
+const noticeComponent = (props) => {
+    return <div className="wb-route-error">
+        {props}
+    </div>;
+};
+export default noticeComponent;
